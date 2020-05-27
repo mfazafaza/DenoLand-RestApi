0f78627960ff1011cca54a700ed414a6eb57c00a
Aulia Mafaza <47191256+mfazafaza@users.noreply.github.com>
2020-05-28 00:07:10 +0700

    Create resource.ts

---
+++ resource.ts
@@ -0,0 +1,16 @@
+//RestApiSederhana.
+const bahasa = new Map<string , any>();
+bahasa.set("1",{
+    id:"1",
+    title:"DenoLand",
+    author:"Mafaza"
+});
+bahasa.set("2",{
+    id:"2",
+    title:"Belajar Deno",
+    author:"Mafaza"
+});
+
+export {
+    bahasa
+}
